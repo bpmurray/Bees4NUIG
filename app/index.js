2d4d280af55abc0697f7fe55a25705db2d64194f
Brendan Murray <brendanpmurray@gmail.com>
2017-03-17 11:02:35 +0000

    Added sample code for the DB

--- app/index.js
+++ app/index.js
@@ -1,1 +1,26 @@
 console.log("OK, I'm here!");
+
+//*** Below is some code to access the DB
+//*** API at: https://github.com/mapbox/node-sqlite3/wiki/API
+//*** var sqlite3 = require('sqlite3').verbose()
+//*** var db = new sqlite3.Database('db/varroarecords.db');
+//*** db.serialize(function() {
+//*** 
+//***   var stmt = db.prepare("INSERT INTO user_info VALUES (?)");
+//***   for (var i = 0; i < 10; i++) {
+//***       stmt.run("Ipsum " + i);
+//***   }
+//***   stmt.finalize();
+//*** 
+//***   db.each("SELECT rowid AS id, info FROM user_info", function(err, row) {
+//***       console.log(row.id + ": " + row.info);
+//***   });
+//*** 
+//***   //Perform SELECT Operation
+//***   db.all("SELECT * from blah blah blah where this="+that,function(err,rows){
+//***      //rows contain values while errors, well you can figure out.
+//***   });
+//***   
+//*** });
+//*** 
+//*** db.close();
