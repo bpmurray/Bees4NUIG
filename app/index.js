a0a889582d05830037b46adcde8c7d5a32af710f
Brendan Murray <brendanpmurray@gmail.com>
2017-04-19 22:40:16 +0100

    Removed old pic. Updated schema to manage logins and security.

--- app/index.js
+++ app/index.js
@@ -24,3 +24,45 @@ console.log("OK, I'm here!");
 //*** });
 //*** 
 //*** db.close();
+
+var crypto = require('crypto');
+var sqlite3 = require('sqlite3');
+
+var db = new sqlite3.Database('db/varroarecords.db');
+
+// ...
+// Hash the password from the user-supplied value
+function hashPassword(password, salt) {
+   var hash = crypto.createHash('sha256');
+   hash.update(password);
+   hash.update(salt);
+   return hash.digest('hex');
+}
+
+passport.use(new LocalStrategy(function(username, password, done) {
+  db.get('SELECT salt FROM users WHERE username = ?', username, function(err, row) {
+    if (!row) return done(null, false);
+    var hash = hashPassword(password, row.salt);
+    db.get('SELECT username, id FROM users WHERE username = ? AND password = ?', username, hash, function(err, row) {
+      if (!row) return done(null, false);
+      return done(null, row);
+    });
+  });
+}));
+
+passport.serializeUser(function(user, done) {
+  return done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  db.get('SELECT id, username FROM users WHERE id = ?', id, function(err, row) {
+    if (!row) return done(null, false);
+    return done(null, row);
+  });
+});
+
+// ...
+
+app.post('/login', passport.authenticate('local', { successRedirect: '/good-login',
+                                                    failureRedirect: '/bad-login' }));
+
