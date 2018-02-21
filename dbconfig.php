b4e651ec7df238c40d87aaf7566eebc24f5fa1c4
Brendan Murray <brendanpmurray@gmail.com>
2018-02-21 16:57:51 +0000

    Added comments re licencing

--- dbconfig.php
+++ dbconfig.php
@@ -1,13 +1,24 @@
 <?php
+/***************************************************************************
+ *  Copyright 2017,2018 Brendan Murray
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ ***************************************************************************/
 
-   //$db_host = "mysql2880int.cp.blacknight.com";
-   //$db_name = "db1454844_registration";
-   //$db_user = "u1454844_root";
-   //$db_pass = "Meabh3Sian";
-   $db_host = "localhost";
+   $db_host = "YourHostName";
    $db_name = "varroarecords";
-   $db_user = "root";
-   $db_pass = "me32316";
+   $db_user = "YourUserName";
+   $db_pass = "YourPassword";
    
    try {
       
